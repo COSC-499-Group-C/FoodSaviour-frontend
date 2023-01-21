f68b1d78d88303505d7794d39ddc9bffa509efe5
tzjg7293 <sydney.tj.fang@gmail.com>
2023-01-20 21:28:57 -0800

    change

--- Old/random/metrics.js
+++ Old/random/metrics.js
@@ -3,30 +3,10 @@ function showTab(n) {
     var x = document.getElementsByClassName("tab");
     x[n].style.display = "block";
 
-    if (n == 0) {
-        document.getElementById("prevBtn").style.display = "none";
-    }
-    if (n < (x.length - 1)) {
-        document.getElementById("nextBtn").innerHTML = '<i class="fa fa-angle-double-right"></i>';
-    }
-    // ss Meta:
-    verbose_name_plural = "Organizations"
 
 
     class OrgGroups(models.Model):
-    //     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    //     group = models.ForeignKey(Organizations, on_delete=models.CASCADE)
-    
-    //     class Meta:
-    //         verbose_name_plural = "OrgGroups"
-    //         unique_together = ["user", "group"]
-    
-    //         ss Meta:
-    //         verbose_name_plural = "Organizations"
-    
-    
-    // class OrgGroups(models.Model):
-    //     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    //     u_delete=models.CASCADE)
     //     group = models.ForeignKey(Organizations, on_delete=models.CASCADE)
     
     //     class Meta:
