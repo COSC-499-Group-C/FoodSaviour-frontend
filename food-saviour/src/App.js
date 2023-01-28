8cacc6a1f15c2db905ef3c6c8634dc774c8d538b
Mitchel Hussack <mitchelhussack@gmail.com>
2023-01-27 17:35:16 -0800

    Revert "Add Login.js and fix a few small things"
    This reverts commit 7ea7333f4f03d13fbb9000c8cc27007ac850e30d.

--- food-saviour/src/App.js
+++ food-saviour/src/App.js
@@ -1,14 +1,15 @@
+import logo from './images/logo.png'
 import './css/App.css';
-import Login from "./Login";
 
 function App() {
   return (
     <div className="">
       <header className="App-header">
         <h1 className="">Food Saviour</h1>
-        <img src={"/images/logo.png"} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <div className = "account-btn">
-          <a href="/Login" className="btn btn-home btn-primary">Login or Register</a>
+          <a href="/login" className="btn btn-home btn-primary">Login</a>
+          <a href="/register" className="register-btn btn btn-home btn-primary">Register</a>
         </div>
       </header>
     </div >
