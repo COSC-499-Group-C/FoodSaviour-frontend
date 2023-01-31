575eb8488331a1d321a79013aa73f8e6da9facd5
Mitchel Hussack <mitchelhussack@gmail.com>
2023-01-31 15:51:52 -0800

    Finished router functionality

---
+++ food-saviour/src/pages/Home.js
@@ -0,0 +1,18 @@
+import '../css/App.css';
+
+function Home() {
+  return (
+    <div className="homepage">
+      <header className="App-header">
+        <h1 className="">Food Saviour</h1>
+        <img src={"/images/logo.png"} className="App-logo" alt="logo" />
+        <div className = "account-btn">
+          <a href="/login" className="btn btn-home btn-primary">Login</a>
+          <a href="/register" className="register-btn btn btn-home btn-primary">Register</a>
+        </div>
+      </header>
+    </div >
+
+  );
+}
+export default Home;
