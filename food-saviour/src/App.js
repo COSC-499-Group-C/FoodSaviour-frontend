5201fa081f4f7ce62e547d5ee04159b6cccac25b
tzjg7293 <tzjg7293@gmail.com>
2023-02-01 10:47:51 -0800

    added routing info to app.js

--- food-saviour/src/App.js
+++ food-saviour/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './css/App.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Sharing from "./pages/Sharing";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Login/>}/>
+                <Route path="/sharing" element={<Sharing/>}/>
             </Routes>
         </Router>
     );
