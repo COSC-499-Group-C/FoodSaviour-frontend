77658508d6fa9d372c76a412fbe05e6e403781aa
Mitchel Hussack <mitchelhussack@gmail.com>
2023-02-03 10:25:05 -0800

    Revert "Merge pull request #57 from COSC-499-Group-C/sharing_page"
    This reverts commit 3e862f9f6c5dfedecf9c0e2746a6269483947c02, reversing
    changes made to 5c4260cddd4a1611fcf3c27f18b9be56be541530.

--- food-saviour/src/App.js
+++ food-saviour/src/App.js
@@ -2,7 +2,6 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './css/App.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Sharing from "./pages/Sharing";
 
 function App() {
     return (
@@ -11,7 +10,6 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Login/>}/>
-                <Route path="/sharing" element={<Sharing/>}/>
             </Routes>
         </Router>
     );
