b03a1de9412ef45bfa69096a5d8f06f6ecd2f7b3
paulgray01 <paulgray01@icloud.com>
2023-02-07 14:24:52 -0800

    added form and styling for tracker page
    added form and styling for tracker page

--- food-saviour/src/pages/Tracker.js
+++ food-saviour/src/pages/Tracker.js
@@ -1,34 +1,134 @@
+import React, { useState } from "react";
 import PieChart from "./PieChart.js";
+import {
+    MDBContainer,
+    MDBDropdown,
+    MDBDropdownMenu,
+    MDBDropdownToggle,
+    MDBDropdownItem,
+    MDBRow,
+    MDBCol,
+    MDBInput,
+    MDBTabsLink,
+    MDBTabsItem,
+    MDBTabs,
+    MDBTabsPane, 
+    MDBTextArea
+}
+    from "mdb-react-ui-kit";
 
 function Tracker() {
-    const data = [{ label: 'Apples', value: 10 }, { label: 'Oranges', value: 20 }];
+    const data = [{ label: "Apples", value: 10 }, { label: "Oranges", value: 20 }, { label: "Bananas", value: 30 }];
 
-    let rad = getText();
-    function getText() {
-        let elem = document.getElementById("ir");
-        let ir = 60;
-        if (elem != null) {
-            ir = parseFloat(elem.value);
+    const [justifyActive, setJustifyActive] = useState("step1");
+
+    const handleJustifyClick = (value) => {
+        if (value === justifyActive) {
+            return;
         }
 
-        return ir;
-    }
+        setJustifyActive(value);
+    };
+
     return (
-        <div>
-            <form>
-                <fieldset>
-                    <input type={"number"} step={"any"} name={"ir"} id={"ir"}/>
-                    <button onClick={getText}/>
-                </fieldset>
-            </form>
+        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+            <div>
+                <h2 className="mb-3">Tracker Page</h2>
+            </div>
+            <div className="p-4 mb-4 shadow-5-strong rounded">
+                <MDBRow tag="form" className="g-3" between>
+                    <MDBCol md="4">
+                        <MDBDropdown>
+                            <MDBDropdownToggle>Waste Type</MDBDropdownToggle>
+                            <MDBDropdownMenu>
+                                <MDBDropdownItem link>Waste Type 1</MDBDropdownItem>
+                                <MDBDropdownItem link>Waste Type 2</MDBDropdownItem>
+                                <MDBDropdownItem link>Waste Type 3</MDBDropdownItem>
+                                <MDBDropdownItem link>Waste Type 4</MDBDropdownItem>
+                                <MDBDropdownItem link>Waste Type 5</MDBDropdownItem>
+                            </MDBDropdownMenu>
+                        </MDBDropdown>
+                    </MDBCol>
+                    <MDBCol md="4">
+                        <MDBInput
+                        name="name"
+                        required
+                        label="Waste Name"
+                        />
+                    </MDBCol>
+                    <MDBCol md="4">
+                        <div className="input-group has-validation">
+                            <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Total Amount"
+                            required
+                            />
+                            <span className="input-group-text">
+                                kg
+                            </span>
+                            <div className="invalid-feedback">Please enter an amount.</div>
+                        </div>
+                    </MDBCol>
+                    <MDBCol md="10">
+                        <MDBTextArea
+                        name="desc"
+                        required
+                        label="Description"
+                        />
+                    </MDBCol>
+                    <MDBCol md="2">
+                        <MDBTabs pills>
+                            <MDBTabsItem>
+                                <MDBTabsLink className="py-4 m-0" onClick={() => handleJustifyClick("step2")} active={justifyActive === "step2"}>
+                                    Next
+                                </MDBTabsLink>
+                            </MDBTabsItem>
+                        </MDBTabs>
+                    </MDBCol>
+                </MDBRow>
+                <MDBTabsPane show={justifyActive === "step2"}>
+                    <MDBRow tag="form" className="g-3 mt-1" show={justifyActive === "step2"}>
+                        <MDBCol md="3">
+                            <MDBInput
+                            name="compost"
+                            required
+                            label="Compost"
+                            />
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBInput
+                            name="compost"
+                            required
+                            label="Donations"
+                            />
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBInput
+                            name="compost"
+                            required
+                            label="Frozen"
+                            />
+                        </MDBCol>
+                        <MDBCol md="3">
+                            <MDBInput
+                            name="compost"
+                            required
+                            label="Landfill"
+                            />
+                        </MDBCol>
+                    </MDBRow>
+                </MDBTabsPane>
+            </div>
+
             <PieChart
-                data={data}
+                data = {data}
                 width = {200}
                 height = {200}
-                innerRadius = {rad}
+                innerRadius = {60}
                 outerRadius = {100}
             />
-        </div>
+        </MDBContainer>
         );
 }
 
