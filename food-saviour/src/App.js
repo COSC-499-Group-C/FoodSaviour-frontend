4bf0e8e68b8db405d7d1c2d3e7090fe26fd13aac
Mitchel Hussack <mitchelhussack@gmail.com>
2023-02-07 19:21:02 -0800

    Basic changes to test out connecting endpoints

--- food-saviour/src/App.js
+++ food-saviour/src/App.js
@@ -2,17 +2,39 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import './css/App.css';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import TestAPI from "./pages/TestAPI"
+import axios from 'axios';
+import React, {Component} from "react";
 
-function App() {
-    return (
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/',
+    headers: {
+        'Authorization': 'Token 2d5c9e287f6af7382d87f980a57d5f2e6ae17f21'
+    }
+})
+
+class App extends Component {
+
+    constructor() {
+        super();
+        api.get('/').then(res => {
+            console.log(res.data);
+        })
+    }
+
+    render() {
+        return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/register" element={<Login/>}/>
+                <Route path="/testapi" element={<TestAPI/>}/>
             </Routes>
         </Router>
     );
+    }
+
 }
 
 export default App;
---
+++ food-saviour/src/pages/TestAPI.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import React, {Component} from "react";
+
+class TestAPI extends Component {
+        render() {
+            return (
+            <div className="testapi">
+              <h1>Hi</h1>
+            </div >
+        );
+        }
+
+}
+export default TestAPI;
+
+// !!!!Ignore this file for now!!!!
