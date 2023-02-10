5aab658a51472c99e2e2b0de399c3a155762d2e4
tzjg7293 <tzjg7293@gmail.com>
2023-02-10 10:52:09 -0800

    updated sharing styling

--- food-saviour/src/pages/Sharing.js
+++ food-saviour/src/pages/Sharing.js
@@ -3,11 +3,11 @@ import '../css/sharing.css';
 
 function Sharing() {
     return (
-        <div>
-            <div>
-                <img className = 'd-flex justify-content-center m-0' src='/images/logo.png' height="50px" />
-                <h1 className='fs-1 text-center mt-5 fw-bold text-black'>Sharing</h1>
-                <p className='text-center text-black'>Have items to share? Need items? You've found the right place!</p>
+        <div className='blue vh-100 overflow-auto'>
+            <div className='text-center'>
+                <img src='/images/logo.png' height="80px" />
+                <h1 className='fs-1 text-center mt-1 fw-bold text-white'>Sharing</h1>
+                <p className='text-center text-white'>Have items to share? Need items? You've found the right place!</p>
             </div>
             {/* <nav className="">
                 <ul className="nav-list">
@@ -18,52 +18,83 @@ function Sharing() {
                     <li><a href="#"></a></li>
                     <li><a href="#"></a></li>
                     <li><a href='#'></a></li>
-                </ul>
+                </ul> */}
 
-                <div className="rightNav">
-                    <input type="text" name="search" id="search" />
-                    <button className="btn btn-sm">Search</button>
-                </div>
-            </nav> */}
+            <div className="left">
+                <input type="text" name="search" id="search" />
+                <button className="btn btn-sm">Search</button>
+            </div>
+            {/* </nav> */}
             <section className='d-flex flex-wrap justify-content-center'>
-                <div className="bg-white p-4 m-3 rounded-4">
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
+                    <div className="org-logo"></div>
+                    <div>
+                        <h1 className="fw-bold fs-4">UBC</h1>
+                        <p className="">Contact info</p>
+                    </div>
+                </div>
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
+                    <div className="org-logo"></div>
+                    <div>
+                        <h1 className="fw-bold fs-4">Food Saviour</h1>
+                        <img className = 'd-flex justify-content-center m-0' src='/images/logo.png' height="50px" />
+                        <p className="">Contact info</p>
+                        <p className="">Email</p>
+                        <p className="">Phone number (optional)</p>
+                    </div>
+                </div>
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
+                    <div className="org-logo"></div>
+                    <div>
+                        <h1 className="fw-bold fs-4">Farmer's Market</h1>
+                        <p className="">Contact info</p>
+                    </div>
+                </div>
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
                     <div className="org-logo"></div>
                     <div>
                         <h1 className="fw-bold fs-4">Organization name</h1>
                         <p className="">Contact info</p>
                     </div>
                 </div>
-                <div className="bg-white p-4 m-3 rounded-4">
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
                     <div className="org-logo"></div>
                     <div>
                         <h1 className="fw-bold fs-4">Organization name</h1>
                         <p className="">Contact info</p>
                     </div>
                 </div>
-                <div className="bg-white p-4 m-3 rounded-4">
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
                     <div className="org-logo"></div>
                     <div>
-                        <h1 className="fw-bold fs-4">Organization name</h1>
+                        <h1 className="fw-bold fs-4">Farmer's Market</h1>
                         <p className="">Contact info</p>
                     </div>
                 </div>
-                <div className="bg-white p-4 m-3 rounded-4">
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
                     <div className="org-logo"></div>
                     <div>
-                        <h1 className="fw-bold fs-4">Organization name</h1>
+                        <h1 className="fw-bold fs-4">Farmer's Market</h1>
                         <p className="">Contact info</p>
                     </div>
                 </div>
-                <div className="bg-white p-4 m-3 rounded-4">
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
                     <div className="org-logo"></div>
                     <div>
-                        <h1 className="fw-bold fs-4">Organization name</h1>
+                        <h1 className="fw-bold fs-4">Farmer's Market</h1>
+                        <p className="">Contact info</p>
+                    </div>
+                </div>
+                <div className="bg-white w-25 p-4 m-3 rounded-4">
+                    <div className="org-logo"></div>
+                    <div>
+                        <h1 className="fw-bold fs-4">Farmer's Market</h1>
                         <p className="">Contact info</p>
                     </div>
                 </div>
 
-                <footer classNameName="footer">
-                    <p classNameName="text-footer">
+                <footer className="footer">
+                    <p className="text-footer">
                     </p>
                 </footer>
             </section >
