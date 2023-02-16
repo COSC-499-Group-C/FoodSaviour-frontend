035cea88216ceec499845e2161ab6f7755ef97ef
paulgray01 <paulgray01@icloud.com>
2023-02-16 14:20:58 -0800

    created navigation bar
    created navigation bar

---
+++ food-saviour/src/addons/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+
+function Navbar() {
+  const [showBasic, setShowBasic] = useState(false);
+
+  return (
+    <MDBNavbar expand="lg" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="#">Food Saviour</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShowBasic(!showBasic)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showBasic}>
+          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="/Home">Home</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="/Tracker">Tracker</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#">Sharing</MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+
+          <MDBBtn>Logout</MDBBtn>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+export default Navbar;
