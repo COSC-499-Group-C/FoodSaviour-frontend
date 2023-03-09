37aef9ad23e60a3cb1b6418c648a7b2f4c8e2d25
Mitchel Hussack <mitchelhussack@gmail.com>
2023-03-08 23:48:58 -0800

    Change names

--- food-saviour/src/components/Sidebar.js
+++ food-saviour/src/components/Sidebar.js
@@ -4,10 +4,10 @@ function Sidebar() {
     return (
         <div className="list-group list-group-light">
             <a href="#" className="list-group-item list-group-item-action px-3 border-0 active" aria-current="true">
-                The current link item</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A second link item</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A third link item</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A fourth link item</a>
+                Date 0</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 1</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 2</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 3</a>
         </div>
     )
 }
