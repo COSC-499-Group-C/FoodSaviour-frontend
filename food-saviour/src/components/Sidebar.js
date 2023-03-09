54b565e08b4da48bd7715c5865c552b715c74b85
Mitchel Hussack <mitchelhussack@gmail.com>
2023-03-08 23:46:13 -0800

    Visuals

--- food-saviour/src/components/Sidebar.js
+++ food-saviour/src/components/Sidebar.js
@@ -1,16 +1,14 @@
-import SidebarMenu from 'react-bootstrap-sidebar-menu';
+
 
 function Sidebar() {
     return (
-        <ul className="list-group list-group-light">
-            <li className="list-group-item px-3 border-0 active" aria-current="true">
-                An active item
-            </li>
-            <li className="list-group-item px-3 border-0">A second item</li>
-            <li className="list-group-item px-3 border-0">A third item</li>
-            <li className="list-group-item px-3 border-0">A fourth item</li>
-            <li className="list-group-item px-3 border-0">And a fifth one</li>
-        </ul>
+        <div className="list-group list-group-light">
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0 active" aria-current="true">
+                The current link item</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A second link item</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A third link item</a>
+            <a href="#" className="list-group-item list-group-item-action px-3 border-0">A fourth link item</a>
+        </div>
     )
 }
 
