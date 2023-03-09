aa30df77c67933a26af5db1a1b41209868d2a902
Mitchel Hussack <mitchelhussack@gmail.com>
2023-03-08 23:39:49 -0800

    Made visuals slightly nicer

--- food-saviour/src/components/Sidebar.js
+++ food-saviour/src/components/Sidebar.js
@@ -2,60 +2,15 @@ import SidebarMenu from 'react-bootstrap-sidebar-menu';
 
 function Sidebar() {
     return (
-        <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-white">
-            <div className="position-sticky">
-                <div className="list-group list-group-flush mx-3 mt-4">
-                    <a
-                        className="list-group-item list-group-item-action py-2 ripple"
-                        aria-current="true"
-                        data-mdb-toggle="collapse"
-                        href="#collapseExample1"
-                        aria-expanded="true"
-                        aria-controls="collapseExample1"
-                    >
-                        <i className="fas fa-tachometer-alt fa-fw me-3"></i><span>Expanded menu</span>
-                    </a>
-                    <ul id="collapseExample1" className="collapse show list-group list-group-flush">
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                    </ul>
-                    <a
-                        className="list-group-item list-group-item-action py-2 ripple"
-                        aria-current="true"
-                        data-mdb-toggle="collapse"
-                        href="#collapseExample2"
-                        aria-expanded="true"
-                        aria-controls="collapseExample2"
-                    >
-                        <i className="fas fa-chart-area fa-fw me-3"></i><span>Collapsed menu</span>
-                    </a>
-                    <ul id="collapseExample2" className="collapse list-group list-group-flush">
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                        <li className="list-group-item py-1">
-                            <a href="" className="text-reset">Link</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <ul className="list-group list-group-light">
+            <li className="list-group-item px-3 border-0 active" aria-current="true">
+                An active item
+            </li>
+            <li className="list-group-item px-3 border-0">A second item</li>
+            <li className="list-group-item px-3 border-0">A third item</li>
+            <li className="list-group-item px-3 border-0">A fourth item</li>
+            <li className="list-group-item px-3 border-0">And a fifth one</li>
+        </ul>
     )
 }
 
