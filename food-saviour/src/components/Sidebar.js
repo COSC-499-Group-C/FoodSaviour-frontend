93c42ff24832b894f13e9d62cfd458573ea6d60d
Mitchel Hussack <mitchelhussack@gmail.com>
2023-03-09 19:00:10 -0800

    Make it so the active link is highlighted

--- food-saviour/src/components/Sidebar.js
+++ food-saviour/src/components/Sidebar.js
@@ -1,15 +1,55 @@
 
 
+import { useState } from 'react';
+
 function Sidebar() {
-    return (
-        <div className="list-group list-group-light">
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0 active" aria-current="true">
-                Date 0</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 1</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 2</a>
-            <a href="#" className="list-group-item list-group-item-action px-3 border-0">Date 3</a>
-        </div>
-    )
+  const [activeLink, setActiveLink] = useState(0);
+
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+
+  return (
+    <div className="list-group list-group-light">
+      <a
+        href="#"
+        className={`list-group-item list-group-item-action px-3 border-0 ${
+          activeLink === 0 ? 'active' : ''
+        }`}
+        onClick={() => handleLinkClick(0)}
+        aria-current="true"
+      >
+        Date 0
+      </a>
+      <a
+        href="#"
+        className={`list-group-item list-group-item-action px-3 border-0 ${
+          activeLink === 1 ? 'active' : ''
+        }`}
+        onClick={() => handleLinkClick(1)}
+      >
+        Date 1
+      </a>
+      <a
+        href="#"
+        className={`list-group-item list-group-item-action px-3 border-0 ${
+          activeLink === 2 ? 'active' : ''
+        }`}
+        onClick={() => handleLinkClick(2)}
+      >
+        Date 2
+      </a>
+      <a
+        href="#"
+        className={`list-group-item list-group-item-action px-3 border-0 ${
+          activeLink === 3 ? 'active' : ''
+        }`}
+        onClick={() => handleLinkClick(3)}
+      >
+        Date 3
+      </a>
+    </div>
+  );
 }
 
 export default Sidebar;
