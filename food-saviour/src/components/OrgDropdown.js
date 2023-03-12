eecb7b91570e416d6e798d9f90c2c3b530ea4da6
Mitchel Hussack <mitchelhussack@gmail.com>
2023-03-12 16:14:39 -0700

    Everything working flow-wise

--- food-saviour/src/components/OrgDropdown.js
+++ food-saviour/src/components/OrgDropdown.js
@@ -12,55 +12,62 @@ import {
 
 function OrgDropdown() {
   const [activeElementType, setActiveElementType] = useState('dropdown');
-  const [name, setName] = useState("");
+  const [name, setName] = useState("Join Organization");
+  const [org_id, setOrg_id] = useState(1);
+  const [orgList, setOrgList] = useState(["Org A", "Org B", "Org C", "Org D"]);
 
-  const orgName = (e, name_id) => {
-    setName (e.target.innerHTML);
-    setName_id(name_id);
-
+  const orgName = (e, org_id) => {
+    setName(e.target.innerHTML);
+    setOrg_id(org_id);
     e.preventDefault();
-}
+  }
 
-  const orgChanged = (e) => {
-    setName(e.target.value);
+  const addOrganization = (e) => {
+    e.preventDefault();
+    const orgNameInput = document.getElementById("org-name-input").value;
+    if (orgNameInput) {
+      setOrgList([...orgList, orgNameInput]);
+      setName(orgNameInput);
+      setActiveElementType("dropdown");
+    }
   }
 
   function dropDown() {
     return (
       <MDBDropdown group className="mb-3">
         <MDBDropdownToggle onClick={(event) => event.preventDefault()}>
-          Join Organization
+          {name}
         </MDBDropdownToggle>
         <MDBDropdownMenu>
-          <MDBDropdownItem link onClick={orgChanged}>Org A</MDBDropdownItem>
-          <MDBDropdownItem link onClick={(event)=> orgName(event,1)}>Produce</MDBDropdownItem>
-          <MDBDropdownItem link onClick={orgChanged}>Org B</MDBDropdownItem>
-          <MDBDropdownItem link onClick={orgChanged}>Org C</MDBDropdownItem>
-          <MDBDropdownItem link onClick={orgChanged}>Org D</MDBDropdownItem>
+          {orgList.map((org, index) => (
+            <MDBDropdownItem key={index} link onClick={(event)=> orgName(event, index + 1)}>
+              {org}
+            </MDBDropdownItem>
+          ))}
         </MDBDropdownMenu>
       </MDBDropdown>
     );
   }
 
   function inputFieldComp() {
-  return (
-    <div className="mb-3">
-      <MDBRow className="align-items-center mb-3">
-        <MDBCol className="flex-grow-1">
-          <MDBInput label="Organization Name" />
-        </MDBCol>
-        <MDBCol>
-          <MDBBtn color="success" type="submit" className="float-end">
-            Add Organization
-          </MDBBtn>
-        </MDBCol>
-      </MDBRow>
-      <div onClick={() => setActiveElementType('dropdown')}>
-        <MDBBtn color="primary">Cancel</MDBBtn>
+    return (
+      <div className="mb-3">
+        <MDBRow className="align-items-center mb-3">
+          <MDBCol className="flex-grow-1">
+            <MDBInput id="org-name-input" label="Organization Name" />
+          </MDBCol>
+          <MDBCol>
+            <MDBBtn color="success" type="submit" className="float-end" onClick={addOrganization}>
+              Add Organization
+            </MDBBtn>
+          </MDBCol>
+        </MDBRow>
+        <div onClick={() => setActiveElementType('dropdown')}>
+          <MDBBtn color="primary">Cancel</MDBBtn>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
   return (
     <div>
@@ -68,8 +75,9 @@ function OrgDropdown() {
         <div className="mb-3">
           {dropDown()}
 
-            <MDBBtn onClick={() => setActiveElementType('input')} color="secondary" className="float-end">Add Organization</MDBBtn>
-
+          <MDBBtn onClick={() => setActiveElementType('input')} color="secondary" className="float-end">
+            Add Organization
+          </MDBBtn>
         </div>
       ) : null}
       {activeElementType === 'input' ? inputFieldComp() : null}
