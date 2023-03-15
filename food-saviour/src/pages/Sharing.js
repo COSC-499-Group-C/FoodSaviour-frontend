716b2fb127c1ce013f3d895c972048f948af7909
Anshul Dhariwal <anshuldhariwal04@gmail.com>
2023-03-14 23:12:08 -0700

    Updated the Sharing page to generate data

--- food-saviour/src/pages/Sharing.js
+++ food-saviour/src/pages/Sharing.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import axiosInstance from "../axios";
-import PieChart from "../components/PieChart.js";
+import React, {useEffect, useState} from "react";
+import "../css/sharing.css";
 import Navbar from "../components/Navbar.js";
+import PieChart from "../components/PieChart";
+import axiosInstance from "../axios";
 import "../css/sharing.css";
 import {
     MDBContainer,
@@ -17,181 +18,152 @@ import {
     MDBCardBody
 }
     from "mdb-react-ui-kit";
+import LoadingComponent from "../util/Loading";
+import Generate from "../util/Generate";
 
-function Sharing() {
-    useEffect(() => {
-        getData();
-        displayData();
-    }, []);
+let wasteId = [];
+let orgId = [];
+function Sharing(props) {
 
-    const [data, setData] = useState([]);
+    const {WasteData, OrgData} = props;
 
-    const getData = () => {
-        axiosInstance
-            .get("trackerData/")
-            .then((response) => {
-                const allData = response.data;
+    const [appState, setAppState] = useState({
+		loading: false,
+		data: [],
+	});
 
-                for (let i = 0; i < allData.length; i++) {
-                    if (allData[i].user === 1) {
-                        setData([allData[i], data]);
-                    }
-                }
-            })
-            .catch(error => console.error(`Error: ${error}`));
-    }
+    const Loading = LoadingComponent(Generate);
 
-    const displayData = () => {
-        for (let i = 0; i < data.length - 1; i++) {
-            setCharts([
-                <MDBCard className="mb-5">
-                    <MDBCardBody>
-                        <p className="mb-4 fw-bold border-bottom">{type}</p>
-                        <PieChart
-                            data={data[i].data}
-                            innerRadius={50}
-                            outerRadius={100}
-                        />
-                        <p className="mt-4 mb-3">{data[i].desc}</p>
-                        <p className="m-0 small float-end">{new Date().toString()}</p>
-                    </MDBCardBody>
-                </MDBCard>,
-                charts]);
+    const handleChange = (e) => {
+        if (e.target.name === 'waste') {
+            if (!wasteId.includes(e.target.value)) {
+                wasteId.push(e.target.value);
+            } else {
+                wasteId = wasteId.filter(function (value) {
+                    return value != e.target.value;
+                });
+            }
+        }
+        if (e.target.name === 'org') {
+            if (!orgId.includes(e.target.value)) {
+                orgId.push(e.target.value);
+            } else {
+                orgId = orgId.filter(function (value) {
+                    return value != e.target.value;
+                });
+            }
+        }
+    };
+
+    const processData = (raw) => {
+        if (raw.length > 0) {
+            let amount1 = 0, amount2 = 0, amount3 = 0, amount4 = 0, amount5 = 0, amount6 = 0, amount7 = 0;
+            let total = 0;
+            for (let i = 0; i < raw.length; i++) {
+                amount1 += parseFloat(raw[i].donations);
+                amount2 += parseFloat(raw[i].compost);
+                amount3 += parseFloat(raw[i].partners);
+                amount4 += parseFloat(raw[i].farmers);
+                amount5 += parseFloat(raw[i].gardens);
+                amount6 += parseFloat(raw[i].landfill);
+                amount7 += parseFloat(raw[i].other);
+                total += (parseFloat(raw[i].donations) + parseFloat(raw[i].compost) + parseFloat(raw[i].partners)
+                    + parseFloat(raw[i].farmers) + parseFloat(raw[i].gardens) + parseFloat(raw[i].landfill)
+                    + parseFloat(raw[i].other));
+            }
+
+            const new_data = [{
+                label: "Donations", value: (100 * amount1 / total).toString(), amount: amount1.toString()},
+                {label: "Compost", value: (100 * amount2 / total).toString(), amount: amount2.toString()},
+                {label: "Partners", value: (100 * amount3 / total).toString(), amount: amount3.toString()},
+                {label: "Farmers", value: (100 * amount4 / total).toString(), amount: amount4.toString()},
+                {label: "Gardens", value: (100 * amount5 / total).toString(), amount: amount5.toString()},
+                {label: "Landfill", value: (100 * amount6 / total).toString(), amount: amount6.toString()},
+                {label: "Other", value: (100 * amount7 / total).toString(), amount: amount7.toString()}];
+
+            setAppState({ loading: false, data: new_data});
         }
     }
 
-    const [type, setType] = useState("");
-    const [total, setTotal] = useState(0);
-    const [desc, setDesc] = useState("no description");
-    const [charts, setCharts] = useState([]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let url = `filteredTrackerData/?`;
+        console.log(wasteId);
+        console.log(orgId);
+        wasteId.forEach((item) => {url += 'waste_type=' + item + '&'});
+        orgId.forEach((item) => {url += 'group=' + item + '&'});
+        axiosInstance.get(url).then((res) =>{
+            processData(res.data);
+        });
 
-    const [percent1, setPercent1] = useState(0), [amount1, setAmount1] = useState(0);
-    const [percent2, setPercent2] = useState(0), [amount2, setAmount2] = useState(0);
-    const [percent3, setPercent3] = useState(0), [amount3, setAmount3] = useState(0);
-    const [percent4, setPercent4] = useState(0), [amount4, setAmount4] = useState(0);
-    const [percent5, setPercent5] = useState(0), [amount5, setAmount5] = useState(0);
-    const [percent6, setPercent6] = useState(0), [amount6, setAmount6] = useState(0);
-    const [percent7, setPercent7] = useState(0), [amount7, setAmount7] = useState(0);
-
-    const wasteType = (e) => {
-        setType(e.target.innerHTML);
-    }
-
-    const totalAmount = (e) => {
-        setTotal(e.target.value);
-    }
-
-    const percentCalc = (e, setPercent, setAmount) => {
-        const percent = (e.target.value / total * 100);
-        setPercent(percent);
-        setAmount(e.target.value);
     };
 
-    const description = (e) => {
-        setDesc(e.target.value);
-    }
-
-    const submitData = (e) => {
-        const newdata = [{ label: "Donations", value: percent1.toString(), amount: amount1.toString() },
-        { label: "Compost", value: percent2.toString(), amount: amount2.toString() },
-        { label: "Partners", value: percent3.toString(), amount: amount3.toString() },
-        { label: "Farmers", value: percent4.toString(), amount: amount4.toString() },
-        { label: "Gardens", value: percent5.toString(), amount: amount5.toString() },
-        { label: "Landfill", value: percent6.toString(), amount: amount6.toString() },
-        { label: "Other", value: percent7.toString(), amount: amount7.toString() }];
-
-        console.log(newdata);
-
-        axiosInstance
-            .post("sharingData/", {
-                data: newdata,
-                user: 1,
-                waste_type: 1,
-                description: desc,
-            })
-            .then((response) => {
-                console.log(response);
-            })
-            .catch(error => console.error(`Error: ${error}`));
-
-        setCharts([
-            <MDBCard className="mb-5">
-                <MDBCardBody>
-                    <p className="mb-4 fw-bold border-bottom">{type}</p>
-                    <PieChart
-                        data={newdata}
-                        innerRadius={50}
-                        outerRadius={100}
-                    />
-                    <p className="mt-4 mb-3">{desc}</p>
-                    <p className="m-0 small float-end">{new Date().toString()}</p>
-                </MDBCardBody>
-            </MDBCard>,
-            charts]);
-        e.preventDefault();
-    }
+    if (!WasteData || !OrgData) return;
 
     return (
         <div className="blue overflow-hidden mw-100 min-vh-100 col">
+            <Navbar></Navbar>
             <div className="w-80 m-auto">
                 <div className="text-center">
-                    <img src="/images/logo.png" height="80px" />
+                    <img src="/images/logo.png" height="80px"/>
                     <h1 className="fs-1 text-center mt-1 fw-bold">Sharing</h1>
                     <p className="text-center">Have items to share? Need items? You've found the right place!</p>
                 </div>
                 <div className="row justify-content-center justify-content-between m-auto">
                     <div className="bg-lblue w-32 p-4 ch-25 rounded-4 overflow-auto">
-                        <h1 className="fw-bold fs-4">Waste category</h1>
-                        <input type="checkbox" /> Produce <br />
-                        <input type="checkbox" /> Frozen food <br />
-                        <input type="checkbox" /> Imperishable <br />
-                        <input type="checkbox" /> Dairy <br />
-                        <input type="checkbox" /> Liquid <br />
-                        <input type="checkbox" /> Liquid <br />
-                        <input type="checkbox" /> Liquid <br />
-                        <input type="checkbox" /> Dairy <br />
-                        <input type="checkbox" /> Liquid <br />
-                        <input type="checkbox" /> Liquid <br />
-                        <input type="checkbox" /> Liquid <br />
+                        <div>
+                            <h1 className="fw-bold fs-4">Waste category</h1>
+                            {WasteData.map((data) => {
+                                return (
+                                    <div key={data.id}>
+                                        <input type="checkbox" onChange={handleChange} name="waste"
+                                               value={data.id}/> {data.name}
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
                     <div className="bg-lblue w-32 ch-25 p-4 rounded-4">
                         <div>
-                            <h1 className="fw-bold fs-4">Organizations</h1>
-                            {/* <img className="d-flex justify-content-center m-0" src="/images/logo.png" height="50px" /> */}
-                            <div>
-                                <input type="checkbox" /> UBC
-                            </div>
-                            <div>
-                                <input type="checkbox" /> Farmer's market
-                            </div>
-                            <div>
-                                <input type="checkbox" /> Food bank
-                            </div>
+                            <h1 className="fw-bold fs-4">Organization</h1>
+                            {OrgData.map((data) => {
+                                return (
+                                    <div key={data.id}>
+                                        <input type="checkbox" onChange={handleChange} name="org" value={data.id} /> {data.name}
+                                    </div>
+                                );
+                            })}
                         </div>
                     </div>
                     <div className="bg-lblue w-32 ch-25 p-4 rounded-4">
                         <div>
                             <h1 className="fw-bold fs-4">Roles</h1>
                             <div>
-                                <input type="checkbox" /> Manager
+                                <input type="checkbox"/> Manager
                             </div>
                             <div>
-                                <input type="checkbox" /> Volunteers
+                                <input type="checkbox"/> Volunteers
                             </div>
                             <div>
-                                <input type="checkbox" /> Supervisor
+                                <input type="checkbox"/> Supervisor
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="row justify-content-center mt-2 mb-2">
-                    <div className='w-25 justify-content-center d-flex btn text-white bg-dblue m-2' onClick={() => {displayData();}}>Generate data</div>
-                    <div className='w-25 justify-content-center d-flex btn text-white bg-dblue m-2' onClick={() => {submitData();}}>Share data</div>
+                    <div className='w-25 justify-content-center d-flex btn text-white bg-dblue m-2' onClick={handleSubmit}>
+                        Generate data
+                    </div>
+                    {/*<div className='w-25 justify-content-center d-flex btn text-white bg-dblue m-2' onClick={() => {*/}
+                    {/*    submitData();*/}
+                    {/*}}>Share data*/}
+                    {/*</div>*/}
                 </div>
                 <div className="d-flex flex-wrap justify-content-center">
                     <div className="bg-lblue w-100 p-4 rounded-4">
                         <div>
                             <h1 className="fw-bold fs-4">Generated graphs</h1>
+                            <Loading isLoading={appState.loading} data={appState.data} />
                         </div>
                     </div>
                     {/* <div className="bg-white w-75 p-4 m-3 rounded-4">
