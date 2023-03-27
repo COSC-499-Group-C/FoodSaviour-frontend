2571e80216857d9e696ac1ceda869efc92707da5
paulgray01 <paulgray01@icloud.com>
2023-03-27 14:37:43 -0700

    landing page styling
    landing page styling

---
+++ food-saviour/src/components/Documentation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Documentation() {
+  return (
+    <div className="btm-right m-3">
+        <a className="text-decoration-none text-white fs-5" href="#top">Documentation</a>
+    </div>
+  );
+}
+
+export default Documentation;
--- food-saviour/src/pages/Home.js
+++ food-saviour/src/pages/Home.js
@@ -1,22 +1,56 @@
 import '../css/App.css';
 import React from "react";
-import { Navigate } from "react-router-dom"
+import Documentation from "../components/Documentation.js";
+import { Navigate } from "react-router-dom";
+import { FaQuestionCircle } from "react-icons/fa";
+import {MDBCol, MDBContainer, MDBRow} from "mdb-react-ui-kit";
 
 function Home() {
     if (localStorage.getItem('refresh_token')) {
         return <Navigate to="/homelogin"/>;
     } else {
-        return (
-            <div className="homepage">
-                <header className="App-header">
-                    <h1 className="">Food Saviour</h1>
-                    <img src={"/images/logo.png"} className="App-logo" alt="logo"/>
-                    <div className="account-btn">
-                        <a href="/login" className="btn btn-home btn-primary">Login or Register</a>
-                    </div>
-                </header>
-            </div>
+        const moreInfo = (e, state) => {
 
+            let left  = e.clientX  + "px";
+            let top  = e.clientY  + "px";
+
+            let info = document.getElementById("info");
+
+            info.style.left = left;
+            info.style.top = top;
+
+            if (state === "on") {
+                info.className = "bg-light position-fixed rounded text-black p-2 m-3 fs-6";
+            } else if (state === "off") {
+                info.className = "d-none position-fixed";
+            }
+        }
+        return (
+            <div className="home">
+            <MDBContainer className="App-header fadein">
+                <MDBRow className="align-items-center">
+                    <MDBCol md="12" className="text-center my-5">
+                        <img src={"/images/foodsaviour_logo.svg"} className="d-inline img-md" alt="logo"/>
+                        <h1 className="d-inline display-1 fw-bold align-middle mx-3">Food Saviour</h1>
+                    </MDBCol>
+                    <MDBCol md="12" className="text-center">
+                        <p className="d-inline align-middle m-0">Dashboard for calculating and avoiding food waste in our food systems.</p>
+                        <span className="align-top px-1 mx-2" onMouseEnter={(event) => moreInfo(event, "on")} onMouseLeave={(event) => moreInfo(event, "off")}>
+                            <FaQuestionCircle/>
+                        </span>
+                        <span className="d-none position-fixed" id="info">
+                            Many non-profits need to manage food waste more quickly, however, few community groups have resources to figure out the ways that food can be diverted from the garbage dump.
+                            <br/>
+                            <b>Food Saviour</b> aims to remedy this by allowing metrics to be calculated and then graphically displayed to illustrate savings calculations.
+                        </span>
+                    </MDBCol>
+                    <MDBCol md="12" className="text-center my-5">
+                        <a href="/login" className="btn btn-home btn-primary">Login or Register</a>
+                    </MDBCol>
+                </MDBRow>
+                <Documentation></Documentation>
+            </MDBContainer>
+            </div>
         );
     }
 }
