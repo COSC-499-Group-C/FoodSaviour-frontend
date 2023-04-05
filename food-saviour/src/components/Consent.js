505928ee373dd27677a1d7ced84b682e0747d45c
paulgray01 <paulgray01@icloud.com>
2023-04-05 14:22:58 -0700

    final fix for consent
    final fix for consent

--- food-saviour/src/components/Consent.js
+++ food-saviour/src/components/Consent.js
@@ -11,7 +11,19 @@ function Consent({ onConsent, onCancel }) {
                         <button type="button" className="btn-close" onClick={onCancel}></button>
                     </MDBModalHeader>
                     <MDBModalBody>
-                        <p>By clicking "I Agree," you consent to our Terms of Service and Privacy Policy.</p>
+                        <p>By using this website, you agree to allow your information to be collected.</p>
+                        <hr/>
+                        <p>
+                            This project gets you active and contributes to Canadian research about social
+                            activities, exercise, wellness, and learning platforms in organizations. The
+                            information you enter will be stored in a secure database that meets Canadian
+                            privacy regulations. Only researchers and professionals will have access to the
+                            information. If you have concerns, please contact UBC Behavioural Research Ethics
+                            Council (BREB) about Barb Marcolin's study #H17-00851, or call her at 250-807-9637.
+                            Faculty of Management, 'Developing Academic-Industry Fast Collaboration Integration
+                            for Strategic Partners'. Please ask Barb Marcolin any further questions.
+                        </p>
+                        <a href="/consent.pdf">Link to digital version of paper consent</a>
                     </MDBModalBody>
                     <MDBModalFooter>
                         <MDBBtn color="secondary" onClick={onCancel}>
